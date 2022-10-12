2842d0523129e4edf880a088e5183c5ff96aeebc
ahmadZaid <ahmaadzaid7@gmail.com>
2022-10-12 17:23:54 +0300

    fix search bar

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,19 +1,19 @@
 <template>
   <from
-    class="max-w-5xl md:flex items-center -translate-y-5 w-full bg-white hidden"
+    class="
+      max-w-5xl
+      md:flex
+      items-center
+      -translate-y-5
+      w-full
+      bg-white
+      hidden
+      rounded-2xl
+      px-10
+    "
   >
-    <div class="relative w-full">
-      <div
-        class="
-          flex
-          absolute
-          inset-y-0
-          left-0
-          items-center
-          pl-3
-          pointer-events-none
-        "
-      >
+    <div class="relative flex w-full">
+      <div class="absolute inset-y-0 -left-7 top-2 pointer-events-none">
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z"
@@ -25,7 +25,15 @@
       <input
         id="title"
         type="text"
-        class="hidden lg:block flex-1 w-full border-none focus:ring-0 mx-7 z-0"
+        class="
+          hidden
+          lg:block
+          flex-[2_2_0%]
+          border-none
+          focus:ring-0
+          z-0
+          w-full
+        "
         placeholder="Filter by title, companies, expertise…"
       />
       <input
@@ -34,7 +42,6 @@
           block
           lg:hidden
           rounded-md
-          flex-1
           w-[100%]
           border-none
           focus:ring-0
@@ -69,7 +76,16 @@
       </div>
       <input
         type="text"
-        class="rounded-md w-[100%] border-none mx-10 px-4 focus:ring-0 z-10"
+        class="
+          rounded-md
+          w-[100%]
+          border-none
+          mx-10
+          px-4
+          focus:ring-0
+          z-10
+          flex-1
+        "
         placeholder="Filter by location…"
       />
     </div>
